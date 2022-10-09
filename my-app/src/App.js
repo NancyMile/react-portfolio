b716bdb10dff12396889d2db511a3a2247af71c8
nancy <smithnancy@outlook.com>
2022-10-09 22:46:08 +1100

    initial componets created and imported to app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+//import components
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Resume from "./components/Resume";
+
 export default function App() {
   return (
     <main>
       <Navbar />
       <About Me />
-      <Profolio />
+      <Portfolio />
       <Contact me />
       <Resume />
     </main>
