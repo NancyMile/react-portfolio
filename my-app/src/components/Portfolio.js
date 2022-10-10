4cb1f748c32b1f2901ab24304b5ff531fa3eac5d
nancy <smithnancy@outlook.com>
2022-10-10 22:04:40 +1100

    images added to portfolio

--- my-app/src/components/Portfolio.js
+++ my-app/src/components/Portfolio.js
@@ -1,4 +1,11 @@
 import React from "react";
+import work0 from '../assets/images/farmers-market-organiser.png'; // gives image path
+import work1 from '../assets/images/work1.png'; // gives image path
+import work2 from '../assets/images/work2.png'; // gives image path
+import work3 from '../assets/images/work3.png'; // gives image path
+import work4 from '../assets/images/work4.png'; // gives image path
+import work5 from '../assets/images/work5.png'; // gives image path
+
 
 export default function Portfolio() {
     return (
@@ -6,61 +13,55 @@ export default function Portfolio() {
                 <div class="col-3" id="work">
                     <h1>Work | </h1>
                 </div>
-                <div class="col-1">
-                        {/* space */}
+                 <div class="col-1">
                 </div>    
                 <div class="col-8">
-                    <div class="card" style="width:800px">
-                        <img class="card-img-top" src="./assets/images/farmers-market-organiser.png" alt="Farmer market">
+                    <div class="card" style={{width:'800px'}}>
+                        <img class="card-img-top" src={work0} alt="Farmer market" />
                         <div class="card-img-overlay">
                             <a href="https://farmers-market-organiser-app.herokuapp.com/" class="btn btn-primary">Visit</a>
                         </div>
                     </div>
                 </div>
-            </div>    
-            {/* end work section */}
-            {/* list prev works */}
-            <div class="row">
-                <div class="col-3">
-                    {/* blank */}
-                </div>
-                <div class="col-1">
-                        {/* space */}
-                </div>    
-                <div class="col-2">
-                    <div class="card mt-3 p-3" style="width:170px">
-                        <img class="card-img-top" src="./assets/images/work1.png" alt="Work1">
-                        <div class="card-img-overlay">
-                            <a href="https://github.com/NancyMile/first-day-repo" class="btn btn-primary">Visit</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-2">
-                    <div class="card mt-3 p-3" style="width:170px">
-                        <img class="card-img-top" src="./assets/images/work2.png" alt="Work2">
-                        <div class="card-img-overlay">
-                            <a href="https://github.com/NancyMile/first-day-repo" class="btn btn-primary">Visit</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-2">
-                    <div class="card mt-3 p-3" style="width:170px">
-                        <img class="card-img-top" src="./assets/images/work3.png" alt="Work3">
-                        <div class="card-img-overlay">
-                            <a href="https://github.com/NancyMile/first-day-repo" class="btn btn-primary">Visit</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-2">
-                    <div class="card mt-3 p-3" style="width:170px">
-                        <img class="card-img-top" src="./assets/images/work4.png" alt="Work4">
-                        <div class="card-img-overlay">
-                            <a href="https://github.com/NancyMile/first-day-repo" class="btn btn-primary">Visit</a>
-                        </div>
-                    </div>
-                </div>
             </div>
-            {/* end list prev works */}
 
+            // <div class="row">
+            //     <div class="col-3">
+            //     </div>
+            //     <div class="col-1">
+            //     </div>
+            //     <div class="col-2">
+            //         <div class="card mt-3 p-3" style={{width:'170px'}}>
+            //             <img class="card-img-top" src={work1} alt="Work1" />
+            //             <div class="card-img-overlay">
+            //                 <a href="https://github.com/NancyMile/first-day-repo" class="btn btn-primary">Visit</a>
+            //             </div>
+            //         </div>
+            //     </div>
+            //     <div class="col-2">
+            //         <div class="card mt-3 p-3" style={{width:'170px'}}>
+            //             <img class="card-img-top" src={work2.png} alt="Work2" />
+            //             <div class="card-img-overlay">
+            //                 <a href="https://github.com/NancyMile/first-day-repo" class="btn btn-primary">Visit</a>
+            //             </div>
+            //         </div>
+            //     </div>
+            //     <div class="col-2">
+            //         <div class="card mt-3 p-3" style={{width:'170px'}}>
+            //             <img class="card-img-top" src={work3} alt="Work3"/>
+            //             <div class="card-img-overlay">
+            //                 <a href="https://github.com/NancyMile/first-day-repo" class="btn btn-primary">Visit</a>
+            //             </div>
+            //         </div>
+            //     </div>
+            //     <div class="col-2">
+            //         <div class="card mt-3 p-3" style={{width:'170px'}}>
+            //             <img class="card-img-top" src={work4} alt="Work4"/>
+            //             <div class="card-img-overlay">
+            //                 <a href="https://github.com/NancyMile/first-day-repo" class="btn btn-primary">Visit</a>
+            //             </div>
+            //         </div>
+            //     </div>
+            // </div>
     );
 }
